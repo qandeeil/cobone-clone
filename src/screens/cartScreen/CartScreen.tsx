97598ac56735222970e62ffe346818b0266aee45
qandeeil <mahmoudqandieel@gmail.com>
2024-08-07 23:19:49 +0300

    build cart screen

--- src/screens/cartScreen/CartScreen.tsx
+++ src/screens/cartScreen/CartScreen.tsx
@@ -1,13 +1,61 @@
 import * as React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {FlatList, Text, TouchableOpacity, View} from 'react-native';
 import styles from './styles';
+import {HeaderCartScreen} from '../../components/headerCartScreen/HeaderCartScreen';
+import {useAppSelector} from '../../store/hooks';
+import {ProductCartCards} from '../../components/cards/productCartCards/ProductCartCards';
 
 type CartScreenProps = {};
 
 export function CartScreen(props: CartScreenProps) {
+  const cartSlice = useAppSelector(state => state.cartSlice);
+  const calculateTotalPrice = () => {
+    return cartSlice.products
+      .reduce((total, item) => {
+        if (item.selectedOption) {
+          const priceAfterDiscount = item.selectedOption.discount
+            ? item.selectedOption.price -
+              (item.selectedOption.price * item.selectedOption.discount) / 100
+            : item.selectedOption.price;
+          return total + priceAfterDiscount * item.selectedOption.quantity;
+        }
+        return 0;
+      }, 0)
+      .toFixed(2);
+  };
+
+  const totalPrice = calculateTotalPrice();
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>CartScreen</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <HeaderCartScreen />
+      <FlatList
+        data={cartSlice.products}
+        keyExtractor={(item, index) => item._id + index}
+        renderItem={({item}) => (
+          <ProductCartCards
+            image={item.image}
+            name={item.name}
+            price={item.selectedOption?.price}
+            discount={item.selectedOption?.discount}
+            product_id={item._id}
+            quantity={item.selectedOption?.quantity}
+            nameSelectedOption={item.selectedOption?.description}
+            optionId={item.selectedOption?._id}
+          />
+        )}
+        contentContainerStyle={styles.contentContainerStyle}
+        showsVerticalScrollIndicator={false}
+      />
+      <View style={styles.containerTotalPrice}>
+        <View style={styles.containerPrice}>
+          <Text style={styles.textTotal}>Total Price</Text>
+          <Text style={styles.textPrice}>AED {totalPrice}</Text>
+        </View>
+        <TouchableOpacity activeOpacity={0.7} style={styles.buttonPay}>
+          <Text style={styles.textButton}>Proceed to Pay</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 }
--- src/screens/cartScreen/styles.ts
+++ src/screens/cartScreen/styles.ts
@@ -1,9 +1,50 @@
 import {StyleSheet} from 'react-native';
+import {moderateScale} from 'react-native-size-matters';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#F5F7F6',
+  },
+  contentContainerStyle: {
+    gap: moderateScale(12, 0.3),
+    paddingTop: moderateScale(12, 0.3),
+    paddingHorizontal: moderateScale(12, 0.3),
+  },
+  containerTotalPrice: {
+    height: moderateScale(110, 0.3),
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: moderateScale(12, 0.3),
+    paddingTop: moderateScale(18, 0.3),
+    backgroundColor: '#fff'
+  },
+  containerPrice: {
+    flexDirection: 'column',
+    gap: moderateScale(5, 0.3),
+  },
+  textTotal: {
+    fontSize: moderateScale(12, 0.3),
+    color: '#222',
+    opacity: 0.8,
+  },
+  textPrice: {
+    fontSize: moderateScale(18, 0.3),
+    color: '#222',
+    fontWeight: 'bold',
+  },
+  buttonPay: {
+    backgroundColor: '#385E83',
+    height: moderateScale(45, 0.3),
+    borderRadius: 12,
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textButton: {
+    color: '#fff',
+    fontSize: moderateScale(14, 0.3),
+    fontWeight: 'bold',
   },
 });
 
