64d9f7c6215d4fff810a2ac6afc8280fb11ea7e4
qandeeil <mahmoudqandieel@gmail.com>
2024-08-07 17:33:15 +0300

    build activities card

--- src/components/cards/activitiesCard/ActivitiesCard.tsx
+++ src/components/cards/activitiesCard/ActivitiesCard.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import {Image, Text, TouchableOpacity, View} from 'react-native';
 import styles from './styles';
+import {useNavigation} from '@react-navigation/native';
 
 type ActivitiesCardProps = {
   name: string;
   price: number;
   image: string;
   background: string | undefined;
+  _id: string;
 };
 
 export function ActivitiesCard({
@@ -14,11 +16,14 @@ export function ActivitiesCard({
   price,
   image,
   background,
+  _id,
 }: ActivitiesCardProps) {
+  const navigation: any = useNavigation();
   return (
     <TouchableOpacity
       style={[styles.container, {backgroundColor: background}]}
-      activeOpacity={0.8}>
+      activeOpacity={0.8}
+      onPress={() => navigation.navigate('Product', {product_id: _id})}>
       <Image source={{uri: image}} style={styles.image} />
       <View style={styles.containerInfo}>
         <Text style={styles.textInfo}>{name}</Text>
