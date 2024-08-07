c8d33d4dfd2bb955ee4b69ef07e8abe5351dc706
qandeeil <mahmoudqandieel@gmail.com>
2024-08-07 13:15:36 +0300

    add background and TouchableOpacity

--- src/components/cards/activitiesCard/ActivitiesCard.tsx
+++ src/components/cards/activitiesCard/ActivitiesCard.tsx
@@ -1,22 +1,29 @@
 import * as React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
 import styles from './styles';
 
 type ActivitiesCardProps = {
   name: string;
   price: number;
   image: string;
-  background: string | undefined
+  background: string | undefined;
 };
 
-export function ActivitiesCard({name, price, image, background}: ActivitiesCardProps) {
+export function ActivitiesCard({
+  name,
+  price,
+  image,
+  background,
+}: ActivitiesCardProps) {
   return (
-    <View style={[styles.container, {backgroundColor: background}]}>
+    <TouchableOpacity
+      style={[styles.container, {backgroundColor: background}]}
+      activeOpacity={0.8}>
       <Image source={{uri: image}} style={styles.image} />
       <View style={styles.containerInfo}>
         <Text style={styles.textInfo}>{name}</Text>
         <Text style={styles.textPrice}>AED {price}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
