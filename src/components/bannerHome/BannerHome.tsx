2144b8d544f13e7eb14abb3c532e641acb84fea6
qandeeil <mahmoudqandieel@gmail.com>
2024-08-07 10:31:44 +0300

    build banners

--- src/components/bannerHome/BannerHome.tsx
+++ src/components/bannerHome/BannerHome.tsx
@@ -1,13 +1,32 @@
 import * as React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native';
 import styles from './styles';
+import bannerJSON from '../../../assets/json/Banners.json';
+import Swiper from 'react-native-swiper';
 
 type BannerHomeProps = {};
 
 export function BannerHome(_props: BannerHomeProps) {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.tester}/>
+      <Swiper
+        style={styles.wrapper}
+        showsButtons={false}
+        autoplay={false}
+        autoplayTimeout={7}
+        dotStyle={styles.dot}
+        paginationStyle={styles.paginationStyle}
+        loop={false}
+        activeDotStyle={styles.activeDot}>
+        {bannerJSON.map((item: any) => (
+          <TouchableOpacity
+            style={styles.slide}
+            key={item._id}
+            activeOpacity={0.9}>
+            <Image source={{uri: item.image}} style={styles.image} />
+          </TouchableOpacity>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 }
--- src/components/bannerHome/styles.ts
+++ src/components/bannerHome/styles.ts
@@ -3,12 +3,38 @@ import {moderateScale} from 'react-native-size-matters';
 
 const styles = StyleSheet.create({
   container: {
+    height: moderateScale(210, 0.3),
+    marginTop: moderateScale(-25, .3)
+  },
+  wrapper: {},
+  slide: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
     paddingHorizontal: moderateScale(12, 0.3),
   },
-  tester: {
-    backgroundColor: 'red',
-    height: moderateScale(150, 0.3),
+  image: {
+    height: moderateScale(160, 0.3),
+    width: '100%',
+    objectFit: 'fill',
     borderRadius: 12,
+  },
+  paginationStyle: {
+    bottom: 0,
+  },
+  dot: {
+    width: moderateScale(24, 0.3),
+    height: moderateScale(5, 0.3),
+    borderRadius: moderateScale(3, 0.3),
+    backgroundColor: '#DEB364',
+    opacity: 0.3,
+  },
+  activeDot: {
+    width: moderateScale(24, 0.3),
+    height: moderateScale(5, 0.3),
+    borderRadius: moderateScale(3, 0.3),
+    backgroundColor: '#DEB364',
+    opacity: 1,
   },
 });
 
